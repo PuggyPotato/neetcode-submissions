e5dba4d112eb8f3592cdc0ebecbba589eee265a1
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-06-22 22:52:20 +0800

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.go
@@ -0,0 +1,11 @@
+func countSeniors(details []string) int {
+	count := 0
+    for _, word := range details {
+		age := (int(word[11])- '0') * 10 + (int(word[12]) -'0')
+		print(age)
+		if age > 60 {
+			count++
+		}
+	}
+	return count
+}
