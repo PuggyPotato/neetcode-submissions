94976de2df14b1dbb0aa03685ed3d9362ea0ad50
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-06-25 21:03:56 +0800

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.go
@@ -0,0 +1,9 @@
+func sortColors(nums []int) {
+    for i :=0; i < len(nums); i++ {
+		for j := i; j >0;j--{
+			if nums[j] < nums[j-1] {
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+			}
+		}
+	}
+}
