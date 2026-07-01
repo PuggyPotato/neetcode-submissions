6a65e159926095843ab917a0876bdfe4d3066080
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-07-02 00:41:27 +0800

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.go
@@ -0,0 +1,19 @@
+func firstMissingPositive(nums []int) int {
+	freq := make([]int,100001)
+	min := 999999999999999
+	for _, val := range nums {
+		if val > 0 {
+			if val < min {
+				min = val
+			}
+			freq[val]++
+		}
+	}
+
+	for i := 1; i < 100001;i++ {
+		if freq[i] == 0 {
+			return i
+		}
+	}
+	return 1
+}
