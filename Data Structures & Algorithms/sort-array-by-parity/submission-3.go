39f8e933d8d71a662d35722e6004b5aa437ebbf2
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-07-19 00:25:01 +0800

    Add: sort-array-by-parity - submission-3

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-3.go
@@ -0,0 +1,16 @@
+func sortArrayByParity(nums []int) []int {
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+
+		if nums[left] % 2 == 0 {
+			left++
+		} else if nums[right] % 2 != 0 {
+			right--
+		} else {
+			nums[left],nums[right] = nums[right], nums[left]
+			right--
+		}
+	}
+	return nums
+}
