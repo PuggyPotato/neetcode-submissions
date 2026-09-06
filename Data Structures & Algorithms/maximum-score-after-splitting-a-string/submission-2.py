813488883b0c0935f189aa1cfbcdae6768e78d77
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-09-07 01:01:30 +0800

    Add: maximum-score-after-splitting-a-string - submission-2

---
+++ Data Structures & Algorithms/maximum-score-after-splitting-a-string/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        count_a = []
+        count_b = []
+
+        count = 0
+        for i in range(len(s) -1):
+            if s[i] == "0":
+                count += 1
+            count_a.append(count)
+            
+        count = 0
+
+        for i in range(len(s) - 1, 0, -1):
+            if s[i] == "1":
+                count += 1
+            count_b.append(count)
+
+        maximum = 0
+        for i in range(len(count_a)):
+            maximum = max(maximum, (count_a[-i] + count_b[i]))
+            
+
+
+        return maximum
+                 
