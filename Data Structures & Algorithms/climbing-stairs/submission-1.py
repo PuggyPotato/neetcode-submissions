7a6734ee5d2940bc3d32f1c834a4af43ef3753e6
PuggyPotato <166225459+PuggyPotato@users.noreply.github.com>
2026-09-26 21:01:03 +0800

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        x = 1
+        y = 2
+
+        for _ in range(2, n):
+            x = x + y
+            x,y = y,x
+
+        return y
+
+
+# 1 = 1
+# 2 = 1 + 1, 2
+# 3 = 1+1+1, 1+2, 2+1
+# 4 = 2 + 2, 1+1+1+1, 2+1+1, 1+2+1, 1+1+2
